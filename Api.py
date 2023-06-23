cff009fc077cce470a601f0737456c7b11fe2d80
Charne Teichert <charne.teichert@gmail.com>
2023-06-23 10:15:58 +0200

    Connected API to our db   Added Categories routes

--- Api.py
+++ Api.py
@@ -1,51 +1,37 @@
 from flask import Flask
 from flask_cors import CORS
 
+from db import db
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def main():
-    return"Girlcode Store API"
+    return "Girlcode Store API"
 
 @app.route('/categories')
-def category_route():
-    tmp_categories = [
-          {
-            "name": 'Furniture',
-            "path": '/category/furniture',
-            "imagePath": 'furnImg',
-          },
-          {
-            "name": 'Handbags',
-            "path": '/category/handbags',
-            "imagePath": 'handbagImg',
-          },
-          {
-            "name": 'Books',
-            "path": '/category/books',
-            "imagePath": 'bookImg',
-          },
-          {
-            "name": 'Tech',
-            "path": '/category/tech',
-            "imagePath": 'techImg',
-          },
-          {
-            "name": 'Sneakers',
-            "path": '/category/sneakers',
-            "imagePath": 'sneakerImg',
-          },
-          {
-            "name": 'Travel',
-            "path": '/category/travel',
-            "imagePath": 'travelImg',
-          },
-        
+def categories_route():
+    tmp_categories = []
 
-    ]
+    cat_instance = db('categories')
+
+    rows = cat_instance.select()
+
+    for row in rows:
+        tmp_cat = {
+            "id": row[0],
+            "name": row[1],
+            "slug": row[2],
+            "desc": row[3],
+            "img": row[4]
+        }
+
+        tmp_categories.append(tmp_cat)
 
     categories_dict = {
-        "categories": tmp_categories 
+        "categories": tmp_categories
     }
 
     return categories_dict
+
--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-"# Girlcode-store-API" 
+# Girlcode-store-API
 
 An API for Girlcode Store built with flask.
 
 ##Install required packages
 
-``````python -m pip install -r requirements.txt ``````
+```python -m pip install -r requirements.txt ```
 
 ##run the api
 
-```````python -m flask --app Api run``````
+```python -m flask --app Api run```
 
 ##run debug mode
-```````python -m flask --app Api --debug run``````
+```python -m flask --app Api --debug run```
 
 ##installed flask-cors
 
-`````python -m pip install Flask-cors`````
+```python -m pip install Flask-cors```
